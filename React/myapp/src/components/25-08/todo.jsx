a3987ee2aae72a5c8125e30c4558383b212ac6f3
khobrekarvinay <khobrekarvinay42@gmail.com>
2024-08-29 11:08:10 +0530

    Update todo.jsx

--- React/myapp/src/components/25-08/todo.jsx
+++ React/myapp/src/components/25-08/todo.jsx
@@ -1,32 +1,32 @@
-// import React, { useState } from "react";
-// import './todo.css';
-// const Todo = () => {
-//     const [todos, setTodos] = useState([]);
-//     const [singleTodo, setSingleTodo] = useState("");
-//     function submitTodo() {
-//         setTodos([...todos, singleTodo]);
-//         setSingleTodo("");
-//     }
-//     function handleInput(event) {
-//         setSingleTodo(event.target.value)
-//     }
-//     console.log(singleTodo, "singleTodo");
-//     console.log(todos, "todos");
+import React, { useState } from "react";
+import './todo.css';
+const Todo = () => {
+    const [todos, setTodos] = useState([]);
+    const [singleTodo, setSingleTodo] = useState("");
+    function submitTodo() {
+        setTodos([...todos, singleTodo]);
+        setSingleTodo("");
+    }
+    function handleInput(event) {
+        setSingleTodo(event.target.value)
+    }
+    console.log(singleTodo, "singleTodo");
+    console.log(todos, "todos");
 
-//     return (
-//         <div className="parent">
-//             <h1 className="h1color"> Your Todo</h1>
-//             <input className="input"
-//                 value={singleTodo}
-//                 onChange={handleInput}
-//                 placeholder="Type your todo here" />
-//             <button className="buttonc" onClick={submitTodo}>Submit Todo</button>
-//             {todos.map((todo) => (
-//                 <h3>{todo}</h3>
-//             ))}
+    return (
+        <div className="parent">
+            <h1 className="h1color"> Your Todo</h1>
+            <input className="input"
+                value={singleTodo}
+                onChange={handleInput}
+                placeholder="Type your todo here" />
+            <button className="buttonc" onClick={submitTodo}>Submit Todo</button>
+            {todos.map((todo) => (
+                <h3>{todo}</h3>
+            ))}
 
-//         </div>
+        </div>
         
-//     )
-// }
-// export default Todo;
+    )
+}
+export default Todo;
